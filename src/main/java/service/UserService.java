68259e8eb4e07994067c8391c4221afde3c38e5d
ForgitL <maydtl@foxmail.com>
2018-03-02 15:40:13 +0800

    delete app

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,9 +1,23 @@
 package service;
 
+import pojo.User;
+
+import java.util.List;
+
 /**
  * @Author：dongtailei
  * @Description：
  * @Date：2018/3/2 14:35
  */
 public interface UserService {
+
+    //注册验证邮箱
+    public boolean checkEmail(String email);
+
+    //登陆验证用户
+    public User checkUser(User user);
+
+    public int getPageCount(int pageSize);
+
+    public List showUser(int pageSize, int pageNow);
 }
--- src/main/java/serviceImpl/UserServiceImpl.java
+++ src/main/java/serviceImpl/UserServiceImpl.java
@@ -1,9 +1,29 @@
 package serviceImpl;
 
+import pojo.User;
+import service.UserService;
+
+import java.util.List;
+
 /**
  * @Author：dongtailei
  * @Description：
  * @Date：2018/3/2 14:35
  */
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService{
+    public boolean checkEmail(String email) {
+        return false;
+    }
+
+    public User checkUser(User user) {
+        return null;
+    }
+
+    public int getPageCount(int pageSize) {
+        return 0;
+    }
+
+    public List showUser(int pageSize, int pageNow) {
+        return null;
+    }
 }
